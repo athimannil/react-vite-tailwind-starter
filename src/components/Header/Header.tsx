ab048adb530c156cc93129c3609d14ef33bcdedf
Muhammed Athimannil <hello@athimannil.com>
2025-10-17 20:18:40 +0200

    feat: Add path alias and clean up Tailwind test component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="header border-b border-gray-200 dark:border-transparent">
-      <div className="container mx-auto flex justify-between px-4 pt-4 pb-3">
+      <div className="container mx-auto flex items-center justify-between px-4 pt-4 pb-3">
         <div className="flex flex-col">
           <h1 className="m-0 text-2xl leading-none font-bold tracking-wide">
             React Starter Kit
